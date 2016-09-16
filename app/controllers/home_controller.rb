e53c4b3a8308c8263a798364a2c90b7597a1d8d7
Skaelv <kevin.antoine@intercom.io>
2016-09-17 00:18:00 +0100

    add widget endpoint

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < IntercomApp::AuthenticatedController
 
   def index
-    # @users_names = @intercom_client.users.find_all(:type=>'users', :per_page => 10, :page => 1, :order => "desc", :sort => "created_at").map(&:name)
   end
 end
--- config/initializers/intercom_app.rb
+++ config/initializers/intercom_app.rb
@@ -1,6 +1,6 @@
 IntercomApp.configure do |config|
-  config.app_key = "d9355dc9-f00a-4514-8216-450e03bf6658"
-  config.app_secret = "69ad8437-6d13-4146-8a63-e88e057978ee"
-  config.hub_secret = "0c026833282dc3c230212d11724b7b1633841764"
+  config.app_key =  ENV['INTERCOM_WIX_CLIENT_ID']
+  config.app_secret =  ENV['INTERCOM_WIX_CLIENT_SECRET']
+  config.hub_secret = ENV['INTERCOM_WIX_HUB_SECRET']
   config.oauth_modal = false
 end
